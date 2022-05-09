4ede75fc90cac072e71d332ad6a72c6da48b5614
yazilimdx <76188846+yazilimdx@users.noreply.github.com>
2022-05-09 19:14:04 +0300

    Initial commit

---
+++ Ozyinelemeli_Fonksiyon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ozyinelemeli_Fonksiyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Faktoriyel(5));
+            Console.ReadKey();
+        }
+
+        public static int Faktoriyel(int n)
+        {
+            if(n==0)
+            {
+                return 1;
+            }
+
+            return n * Faktoriyel(n - 1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Ozyinelemeli_Fonksiyon
+ Ozyineleme
